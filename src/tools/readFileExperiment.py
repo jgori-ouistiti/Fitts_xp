10d1fab4db0d67e9e0b658efb7bce712f88d316b
hedgehogpotatoes <lingjuliette@gmail.com>
2022-04-29 18:27:58 +0200

    fichier config peut contenir cible en cercle et cible densite

--- src/tools/readFileExperiment.py
+++ src/tools/readFileExperiment.py
@@ -1,3 +1,4 @@
+from tkinter import Widget
 import colors as Colors
 import sys
 sys.path.append('../class')
@@ -29,6 +30,46 @@ def readFileExperiment(filename, WIDTH, HEIGHT):
             nbMouv = int(parameters[3].strip("\n"))
             print("Nb mouvement :", nbMouv)
             list_experiences.append(Experiment(targets, exp_name, exp_id, nbMouv))
+            print("Experience ajouter !")
+
+        elif type_experience == 'cible':
+            print("C'est Cible libre !")
+            disposition = parameters[2] 
+            if disposition == "cercle":
+                type_cible = parameters[3]
+                if type_cible == "cercle":
+                    rayon_D = int(parameters[4])        #correspond au rayon pour la disposition en cercle
+                    rayon_cercle = int(parameters[5])   #correspond au rayon d'une cible
+                    nbCible = int(parameters[6])
+                    pos = parameters[7]
+                    i = 7
+                    if pos[0]=="(":
+                        pos = pos.strip("(),")
+                        pos = (int(pos[0]),int(pos[1]))
+                        print("position : ", pos)
+                        i = 8
+                    else : 
+                        pos = (WIDTH/2, HEIGHT/2)             
+                    targets = make_circle_target_list(pos, rayon_D, nbCible, Colors.GREEN, rayon_cercle)
+                    
+                    nbMouv = int(parameters[i].strip("\n"))
+                    list_experiences.append(Experiment(targets, type_cible, exp_id, nbMouv))
+                #elif type_cible=="rect":
+            elif disposition == "densite":
+                dimensions = (WIDTH, HEIGHT)
+
+                center = parameters[3]
+                center = center.strip("(),")
+                center = (int(center[0]),int(center[1]))
+
+                ID = int(parameters[4])
+                A  = int(parameters[5])
+                p  = float(parameters[6])
+                jmax = int(parameters[7])
+                targets = make_2D_distractor_target_list(dimensions, center, ID, A, p, Colors.BLACK, jmax)
+                nbMouv = int(parameters[8].strip("\n"))
+                list_experiences.append(Experiment(targets, disposition, exp_id, nbMouv))
+            
             print("Experience ajouter !")
 
         print("=========")
