52422229f183a0fd3223f23a1daaa0f75d85eb13
Quentin BELLUT <bellut@isir.upmc.fr>
2022-06-15 13:54:18 +0200

    added variable that saves all the drawings

--- src/editor.py
+++ src/editor.py
@@ -1,13 +1,15 @@
 import tkinter as tk
 from tkinter import colorchooser
+from PIL import ImageTk, Image
 import math
 
 root = tk.Tk()
 x_shift = 100
 y_shift = 100
+toolbar_width = 25
 
 screen_width = root.winfo_screenwidth() - 2*x_shift
-screen_height = root.winfo_screenheight() - 2*y_shift
+screen_height = root.winfo_screenheight() - 2*y_shift - toolbar_width
 
 class MFrame(tk.Frame):
     def __init__(self, root, title="Experiment editor"):
@@ -15,23 +17,35 @@ class MFrame(tk.Frame):
         
         self.master.title(title)
         self.pack(fill = tk.BOTH, expand = 1)
-        self.canvas = tk.Canvas(self)
-        self.canvas.pack(fill = tk.BOTH, expand = 1)
         
         #List of different shapes followed by the index of the selected shape
         self.shapes = [['circle', 'rectangle'],0]
+        self.shapes_ind = {self.shapes[0][k]:k for k in range(len(self.shapes[0]))}
         self.current_drawing = None
         self.outline = "#000"
         self.fill = "#fff"
         self.width = 2
+        self.sensibility = 0.5 #used to making precise shapes while holding left mouse button and moving the mouse
         
         #List of all the shapes drawed on screen.
         self.drawings = []
         
-        #Binding shortcuts
-        root.bind('<Button-1>', self.clicked)
-        root.bind('<B1-Motion>', self.motion)
-        root.bind('<ButtonRelease-1>', self.released)
+        #Tools canvas part
+        self.tools  = tk.Canvas(self, height = toolbar_width)
+        sq_button_image = Image.open('images/square_button.png')
+        sq_button_image = sq_button_image.resize((25,25), Image.Resampling.LANCZOS)
+        rectangle_button = tk.Button(self.tools, image = ImageTk.PhotoImage(sq_button_image) , command = self.set_rectangle_shape, anchor = tk.W, borderwidth=0)
+        self.tools.create_window(0,0,window = rectangle_button, anchor = tk.NW)
+        self.tools.pack(fill = tk.BOTH)
+        
+        #Shape canvas part
+        self.canvas = tk.Canvas(self)
+        self.canvas.pack(fill = tk.BOTH, expand = 1)
+        self.canvas.bind('<Button-1>', self.clicked)
+        self.canvas.bind('<B1-Motion>', self.motion)
+        self.canvas.bind('<ButtonRelease-1>', self.released)
+        
+        #Binding shortcuts in both
         root.bind('<Key>', self.key_pressed)
         root.bind('<Control-Key-z>', self.undo)
         root.bind('<Control-Key-Z>', self.undo)
@@ -47,7 +61,7 @@ class MFrame(tk.Frame):
         if len(self.drawings) == 0:
             return
         drawing = self.drawings.pop()
-        self.canvas.delete(drawing)
+        self.canvas.delete(drawing[0])
     
     def clicked(self, event):
         #called when using left mouse button
@@ -56,6 +70,13 @@ class MFrame(tk.Frame):
     
     def get_current_shape(self):
         return (self.shapes)[0][self.shapes[1]]
+    
+    def set_rectangle_shape(self):
+        print("rectangle :",self.shapes)
+        self.shapes[1] = self.shapes_ind['rectangle']
+    
+    def set_circle_shape(self):
+        self.shapes[1] = self.shapes_ind['circle']
     
     def motion(self, event):
         #Drawing dynamically the current shape the user is drawing
@@ -66,7 +87,7 @@ class MFrame(tk.Frame):
         
         #circle case
         if self.get_current_shape() == 'circle':
-            r = math.sqrt((self.x1 - self.x0)**2 + (self.y1 - self.y0)**2)
+            r = self.sensibility * math.sqrt( (self.x1 - self.x0)**2 + (self.y1 - self.y0)**2 )
             self.current_drawing = self.draw_circle(self.x0, self.y0, r)
         #rectangle case
         if self.get_current_shape() == 'rectangle':
@@ -74,7 +95,14 @@ class MFrame(tk.Frame):
         
     def released(self, event):
         #called when the user release left mouse button
-        self.drawings.append(self.current_drawing)
+        self.drawings.append((self.current_drawing,{'shape':self.get_current_shape(),\
+                                                    'x0':self.x0,\
+                                                    'y0':self.y0,\
+                                                    'x1':self.x1,\
+                                                    'x2':self.x1,\
+                                                    'border_color':self.outline,\
+                                                    'fill_color':self.fill,\
+                                                    'width':self.width}))
         self.current_drawing = None
         
     def next_shape(self):
@@ -102,6 +130,9 @@ class MFrame(tk.Frame):
             print("Select new color")
             self.fill = self.new_color()[1]
             print("Color selected : ", self.fill)
+            
+        if event.keysym == 'a':
+            print([k[1] for k in self.drawings])
 
 def main():
     frame = MFrame(root)
