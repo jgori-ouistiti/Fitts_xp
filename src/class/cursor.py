8196e937f8c40269370211c41a1953ba7bdf3b31
Quentin BELLUT <bellut@isir.upmc.fr>
2022-07-01 11:56:32 +0200

    changed default cursor visual

--- src/class/cursor.py
+++ src/class/cursor.py
@@ -10,7 +10,7 @@ class Cursor(Drawable, Listener):
         self.HEIGHT = HEIGHT
         self.visible = visible
         self.cursorImage = pygame.image.load(cursorImage)
-        self.cursorImage = pygame.transform.scale(self.cursorImage, (50, 50))
+        self.cursorImage = pygame.transform.scale(self.cursorImage, (13, 20))
         self.imagePath = cursorImage
         
     def move(self, dx, dy):
