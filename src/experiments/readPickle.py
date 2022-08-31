48e823959a0e3f87d215d8c061a88fd1693d82c9
NeSTPoPs <quentin.bellut95130@gmail.com>
2022-09-01 01:54:27 +0200

    added readPickle to read chart of experiment in pickle files

---
+++ src/experiments/readPickle.py
@@ -0,0 +1,39 @@
+import pickle as pkl
+
+FILE = 'complete_experiment_design.pickle'
+nb_of_movements = 20
+
+def makeExperiments(data):
+    for P, exps in data.items():
+        experiment_file = 'random True\n'
+        id_fitts = 0
+        id_pvp = 0
+        for id_exp, exp in exps.items():
+            for name in exp:
+                for D, W in exp[name]:
+                    D = int(D)
+                    W = int(W)
+                    if name[:5] == 'Fitts':
+                        if id_fitts %2 == 0:
+                            sens = ' AH'
+                        else:
+                            sens = ' H'
+                        experiment_file += 'fitts_exp'+str(id_fitts)+' circle '+str(D) +' '+str(W)+' ' + str(nb_of_movements)+sens+'\n'
+                        id_fitts += 1
+                    elif name[:3] == 'PVP':
+                        experiment_file += 'pvp_exp'+str(id_pvp)+' random '+str(D) +' 3 ' + str(nb_of_movements)+'\n'
+                        id_pvp += 1
+        file = open((P+'.txt'), 'w')
+        file.write(experiment_file)
+        file.close()
+                    
+
+def main():
+    file = open(FILE,'rb')
+    data = pkl.load(file)
+    makeExperiments(data)
+    file.close()
+    return 0
+    
+if __name__ == '__main__':
+    main()
